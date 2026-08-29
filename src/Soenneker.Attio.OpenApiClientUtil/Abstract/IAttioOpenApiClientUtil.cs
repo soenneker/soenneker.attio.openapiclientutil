2ad7bbb9882593cef71547446c5d28cb0609d3a2
soenneker <jake@soenneker.com>
2026-08-29 14:16:46 -0500

    Improve public API XML documentation

--- src/Soenneker.Attio.OpenApiClientUtil/Abstract/IAttioOpenApiClientUtil.cs
+++ src/Soenneker.Attio.OpenApiClientUtil/Abstract/IAttioOpenApiClientUtil.cs
@@ -2,13 +2,16 @@ using Soenneker.Attio.OpenApiClient;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
-
 namespace Soenneker.Attio.OpenApiClientUtil.Abstract;
-
 /// <summary>
 /// Exposes a cached OpenAPI client instance.
 /// </summary>
 public interface IAttioOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Returns the configured attio OpenAPI Client used by the Attio OpenAPI Client.
+    /// </summary>
+    /// <param name="cancellationToken">Token used to cancel the operation.</param>
+    /// <returns>A task whose result is the requested attio OpenAPI Client.</returns>
     ValueTask<AttioOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
--- src/Soenneker.Attio.OpenApiClientUtil/Registrars/AttioOpenApiClientUtilRegistrar.cs
+++ src/Soenneker.Attio.OpenApiClientUtil/Registrars/AttioOpenApiClientUtilRegistrar.cs
@@ -13,6 +13,8 @@ public static class AttioOpenApiClientUtilRegistrar
     /// <summary>
     /// Adds <see cref="AttioOpenApiClientUtil"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddAttioOpenApiClientUtilAsSingleton(this IServiceCollection services)
     {
         services.AddAttioOpenApiHttpClientAsSingleton()
@@ -24,6 +26,8 @@ public static class AttioOpenApiClientUtilRegistrar
     /// <summary>
     /// Adds <see cref="AttioOpenApiClientUtil"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddAttioOpenApiClientUtilAsScoped(this IServiceCollection services)
     {
         services.AddAttioOpenApiHttpClientAsSingleton()
